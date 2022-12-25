a48614e8597e0d926b93d739ca8f3260bcfdb150
Maxim Makovskiy <makovskiyms@gmail.com>
2022-12-25 20:38:39 +0600

    Remove ftplugin, colorscheme and make it compatible with Windows

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,12 +1,15 @@
-vim.cmd[[colorscheme onehalfdark]]
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.termguicolors = true
-vim.opt.cursorline = true
-vim.opt.expandtab = true
+if not vim.g.vscode then
+  vim.cmd.colorscheme('quiet')
+  vim.opt.number = true
+  vim.opt.relativenumber = true
+  vim.opt.termguicolors = true
+  vim.opt.cursorline = true
+  vim.opt.expandtab = true
+  vim.opt.tabstop = 2
+  vim.opt.shiftwidth = 0
+  vim.opt.softtabstop = -1
+  vim.opt.scrolloff = 5
+  vim.opt.mouse = 'a'
+  vim.opt.colorcolumn = '80'
+end
 vim.opt.clipboard = 'unnamedplus'
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 0
-vim.opt.softtabstop = -1
-vim.opt.scrolloff = 5
-vim.opt.mouse = 'a'
