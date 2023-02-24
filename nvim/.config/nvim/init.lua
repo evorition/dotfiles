5dc87f0ad90ebd0762acb50541d7ddcb46aee57b
Maxim Makovskiy <makovskiyms@gmail.com>
2023-02-24 16:30:16 +0600

    Small changes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,15 +1,16 @@
-if not vim.g.vscode then
-  vim.cmd.colorscheme('quiet')
-  vim.opt.number = true
-  vim.opt.relativenumber = true
-  vim.opt.termguicolors = true
-  vim.opt.cursorline = true
-  vim.opt.expandtab = true
-  vim.opt.tabstop = 2
-  vim.opt.shiftwidth = 0
-  vim.opt.softtabstop = -1
-  vim.opt.scrolloff = 5
-  vim.opt.mouse = 'a'
-  vim.opt.colorcolumn = '80'
-end
+vim.cmd [[ colorscheme shine ]]
+vim.cmd [[ syntax off ]]
+vim.cmd [[ set guicursor=n:blinkon0 ]]
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 0
+vim.opt.softtabstop = -1
+vim.opt.scrolloff = 5
+vim.opt.mouse = 'a'
+vim.opt.colorcolumn = '80'
+vim.opt.shada = None
 vim.opt.clipboard = 'unnamedplus'
